79d52ad904c2636e32233398d8a64b432337829f
Guilherme Vinhas <guilhermejosevinhas@gmail.com>
2023-03-13 14:11:37 -0300

    "Added get request for envelope routes"

--- src/routes.js
+++ src/routes.js
@@ -4,15 +4,20 @@ const envelopes = require('./envelopes')
 
 envelopeRouter.post('/', (req,res,next) => {
     try{
-        console.log(req.body)
         envelopes[req.body.envelope] = 0
-        console.log(envelopes)
         res.status(201).send('Created')}
     catch(err){
         res.status(500).send('Internal Server Error')
     }
 })
 
+envelopeRouter.get('/', (req,res,next) => {
+    try{
+        res.status(200).send(envelopes)   
+    }catch(err){
+        res.status(500).send()
+    }
+})
 
 
 
