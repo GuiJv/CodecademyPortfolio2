3111f85c9ea40aee971660472cebce78d95b4b12
Guilherme Vinhas <guilhermejosevinhas@gmail.com>
2023-03-13 14:44:32 -0300

    "Added individual get for envelopes"

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,32 @@
 const express = require('express')
+const { groceryBudget } = require('./envelopes')
 const envelopeRouter = express.Router()
 const envelopes = require('./envelopes')
 
-envelopeRouter.post('/', (req,res,next) => {
-    try{
-        envelopes[req.body.envelope] = 0
-        res.status(201).send('Created')}
-    catch(err){
-        res.status(500).send('Internal Server Error')
+
+envelopeRouter.param('envelope', (req,res,next, envelope) =>{
+    if(envelopes.hasOwnProperty(envelope)){
+        console.log(envelopes[envelope])
+        req.envelope = envelopes[envelope]
+        next()
+    }else{
+        res.status(404).send('Not Found Envelope')
     }
 })
 
+
+envelopeRouter.post('/', (req,res,next) => {
+    envelopes[req.body.envelope] = 0
+    res.status(201).send('Created')}
+)
+
 envelopeRouter.get('/', (req,res,next) => {
-    try{
         res.status(200).send(envelopes)   
-    }catch(err){
-        res.status(500).send()
-    }
+})
+
+envelopeRouter.get('/:envelope/', (req,res,next) => {
+    const envelope = req.envelope
+    res.status(200).send(`${envelope}`)
 })
 
 
